2fa967a8faee49a0667b44612358fc5f5d59cf91
Tirumala-Sai-Teja <tirumalasaiteja988@gmail.com>
2021-07-05 21:25:07 +0530

    auto close collapse

--- vidyanikethan_scrippt.js
+++ vidyanikethan_scrippt.js
@@ -5,15 +5,15 @@ AOS.init({
 // navbarNav
 
 const myCollapsible = document.getElementById('navbarNav');
-
+// const myOffcanvas=document.getElementById('offcanvasTop')
 
 const bsCollapse = new bootstrap.Collapse(myCollapsible, { toggle: false })
-
+// const bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
 
 function scrolltopele() {
     screenTop();
 }
 
 function hideCollapse() {
-    bsCollapse.hide();
+    myCollapsible.classList.toggle('show');
 }
