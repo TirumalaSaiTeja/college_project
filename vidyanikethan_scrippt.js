415fa91c9ab98b97d5d9c660f7a860ef1004b9d2
Tirumala-Sai-Teja <tirumalasaiteja988@gmail.com>
2021-07-05 18:49:50 +0530

    auto close collapse element

--- vidyanikethan_scrippt.js
+++ vidyanikethan_scrippt.js
@@ -1,3 +1,17 @@
 AOS.init({
     delay:250
 });
+
+// navbarNav
+
+const myCollapsible = document.getElementById('navbarNav');
+
+
+const bsCollapse = new bootstrap.Collapse(myCollapsible, { toggle: false })
+
+
+
+
+function hideCollapse() {
+    bsCollapse.hide();
+}
